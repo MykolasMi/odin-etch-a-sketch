b5f6258965db91938f599702f574bc4d49baaae1
MykolasMi <mykolas.miskinisss@gmail.com>
2022-06-19 14:48:07 +0300

    add working change grid button

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odin-etch-a-sketch
+Needs a set size for the grid. Need to fix dissapearing grid. Optionally add rainbow mode.
--- jscript.js
+++ jscript.js
@@ -1,14 +1,49 @@
 const container = document.getElementById('container');
 
-/*const block = document.createElement('div');
-container.appendChild(block);
-block.style.cssText = 'display: flex; border-width: 5px; border-color: black; border-style: solid;'
-*/
+let number = 16;
 
-
-for (let i =0; i<256; i++) {
+for (let i =0; i<number*number; i++) {
     const block = document.createElement('div');
     container.appendChild(block);
     block.className = "block";
-    //block.style.cssText = 'display: flex; border-width: 5px; border-color: black; border-style: solid;';
+    
+    block.addEventListener("mouseover", event => {
+        block.className = 'blockActive';
+    })
+    
+    block.addEventListener("mouseout", event => {
+        //block.classList.remove('blockActive');
+        //block.className = 'block';
+    })
+}
+
+const button = document.getElementById('button');
+
+button.addEventListener("click", function(e) {
+    for (let i =0; i<number*number; i++) {
+        container.firstElementChild.remove();
+    }
+    number = prompt("Please enter a grid size (Maximum 100)");
+    if (number>100) {
+        number=0;
+    }
+    createGrid(number);
+    container.style.setProperty("--grid-template-value", number);
+})
+
+function createGrid (number) {
+    for (let i =0; i<number*number; i++) {
+        const block = document.createElement('div');
+        container.appendChild(block);
+        block.className = "block";
+        
+        block.addEventListener("mouseover", event => {
+            block.className = 'blockActive';
+        })
+        
+        block.addEventListener("mouseout", event => {
+            //block.classList.remove('blockActive');
+            //block.className = 'block';
+        })
+    }
 }
